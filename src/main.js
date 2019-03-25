243951feb0654a7c64d01910ecdf50a65096ea0a
Dragosh <stefanescu.dragos@gmail.com>
2019-03-25 07:07:29 +0100

    initial commit for todo workshop

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import './assets/css/base.css'
+import './assets/css/index.css'
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
